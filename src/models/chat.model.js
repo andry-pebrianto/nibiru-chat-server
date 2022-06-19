a9decd296189e39e545011805fbcdef5e32d4725
Andry Pebrianto <andrypeb227@gmail.com>
2022-06-20 00:29:51 +0700

    Menambahkan is_deleted pada response list chat

--- src/models/chat.model.js
+++ src/models/chat.model.js
@@ -42,7 +42,7 @@ module.exports = {
   list: (sender, receiver) => {
     return new Promise((resolve, reject) => {
       db.query(
-        `SELECT userSender.photo AS photo, chats.date, chats.id, chats.chat, userSender.id AS sender_id, userReceiver.id AS receiver_id FROM chats LEFT JOIN users AS userSender ON chats.sender=userSender.id LEFT JOIN users AS userReceiver ON chats.receiver=userReceiver.id WHERE (sender='${sender}' AND receiver='${receiver}') OR (sender='${receiver}' AND receiver='${sender}')`,
+        `SELECT userSender.photo AS photo, chats.date, chats.id, chats.chat, chats.is_deleted, userSender.id AS sender_id, userReceiver.id AS receiver_id FROM chats LEFT JOIN users AS userSender ON chats.sender=userSender.id LEFT JOIN users AS userReceiver ON chats.receiver=userReceiver.id WHERE (sender='${sender}' AND receiver='${receiver}') OR (sender='${receiver}' AND receiver='${sender}')`,
         (err, res) => {
           if (err) {
             reject(err);
